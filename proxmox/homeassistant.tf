b59f49d56477f769ec5da8545e095445106ad8c4
Kalitsune <git@kalitsune.net>
2026-08-25 10:57:42 +0200

    feat: replace HAOS image download with SSH-based disk import provisioner
    Switches from proxmox_download_file to a remote-exec provisioner that
    downloads, decompresses, and imports the disk directly on the Proxmox
    host, avoiding provider limitations with qcow2.xz images.

--- proxmox/homeassistant.tf
+++ proxmox/homeassistant.tf
@@ -1,18 +1,13 @@
-resource "proxmox_download_file" "haos_image" {
-  node_name    = var.proxmox_node
-  content_type = "iso"
-  datastore_id = var.proxmox_iso_storage
-
-  file_name = "haos.img"
-  url       = "https://github.com/home-assistant/operating-system/releases/download/18.2/haos_ova-18.2.qcow2.xz"
+locals {
+  proxmox_host    = regex("https?://([^:/]+)", var.proxmox_endpoint)[0]
+  haos_vm_id      = 201
+  haos_url        = "https://github.com/home-assistant/operating-system/releases/download/18.2/haos_ova-18.2.qcow2.xz"
+  haos_cache_file = "/var/lib/vz/template/cache/haos_ova-18.2.qcow2.xz"
 }
 
-# After first apply, import the boot disk manually on the Proxmox host:
-#   qm importdisk 201 /var/lib/vz/template/iso/haos.img local-lvm --format raw
-# Then in the Proxmox GUI: Hardware → unused disk → Edit → set as scsi0, enable boot order.
 resource "proxmox_virtual_environment_vm" "homeassistant" {
   node_name = var.proxmox_node
-  vm_id     = 201
+  vm_id     = local.haos_vm_id
   name      = "home-assistant"
 
   on_boot = true
@@ -20,6 +15,10 @@ resource "proxmox_virtual_environment_vm" "homeassistant" {
 
   machine = "q35"
   bios    = "ovmf"
+
+  agent {
+    enabled = true
+  }
 
   cpu {
     cores = 2
@@ -41,9 +40,46 @@ resource "proxmox_virtual_environment_vm" "homeassistant" {
     model  = "virtio"
   }
 
-  scsi_hardware = "virtio-scsi-single"
+  serial_device {}
+
+  scsi_hardware = "virtio-scsi-pci"
 
   vga {
     type = "std"
+  }
+
+  lifecycle {
+    ignore_changes = [disk, boot_order]
+  }
+}
+
+resource "terraform_data" "haos_disk_import" {
+  triggers_replace = {
+    vm_id = proxmox_virtual_environment_vm.homeassistant.id
+    url   = local.haos_url
+  }
+
+  connection {
+    type  = "ssh"
+    host  = local.proxmox_host
+    user  = "root"
+    agent = true
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "set -e",
+      "if qm config ${local.haos_vm_id} | grep -q '^scsi0:'; then exit 0; fi",
+      "mkdir -p /var/lib/vz/template/cache",
+      "if [ ! -s '${local.haos_cache_file}' ] || ! xz -t '${local.haos_cache_file}' 2>/dev/null; then wget -q -O '${local.haos_cache_file}' '${local.haos_url}'; fi",
+      "IMG=$(mktemp /tmp/haos.XXXXXX.qcow2)",
+      "xz -dc '${local.haos_cache_file}' > \"$IMG\"",
+      "qm importdisk ${local.haos_vm_id} \"$IMG\" ${var.proxmox_storage} --format raw",
+      "rm -f \"$IMG\"",
+      "DISK=$(qm config ${local.haos_vm_id} | grep '^unused0:' | awk '{print $2}')",
+      "qm set ${local.haos_vm_id} --scsi0 \"$DISK,ssd=1,discard=on\"",
+      "qm set ${local.haos_vm_id} --boot order=scsi0",
+      "qm resize ${local.haos_vm_id} scsi0 32G",
+    ]
   }
 }
